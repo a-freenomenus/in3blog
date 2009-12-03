63a5e84949b779d134bc307fc261c7005d1c7a50
uzzz <max.gilinsky@gmail.net>
2009-12-03 16:20:24 +0200

    posts pagination in tag page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,5 +5,6 @@ class TagsController < ApplicationController
   
   def show
     @tag = Tag.find_by_name(params[:name])
+    @posts = Post.tagged_with(@tag).paginate :page => params[:page], :per_page => 20
   end
 end
