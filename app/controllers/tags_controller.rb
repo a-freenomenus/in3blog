5cd3f4185578b0eecb0597eee3360f0e62f54d6d
uzzz <max.gilinsky@gmail.net>
2009-12-04 09:53:44 +0200

    highlighting of tag in tag page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,4 @@
 class TagsController < ApplicationController
-  def index
-    @tags = Tag.all
-  end
-  
   def show
     @tag = Tag.find_by_name(params[:name])
     @posts = Post.tagged_with(@tag).paginate :page => params[:page], :per_page => 20
