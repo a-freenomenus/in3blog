6456af3fbed1516a9c4d26db7897c7ca882d7df8
uzzz <max.gilinsky@gmail.net>
2009-12-07 16:37:24 +0200

    player helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
     show_volume = options[:show_volume] || 1
     %(
       <object type="application/x-shockwave-flash" data="/player/player_mp3_maxi.swf" width="200" height="20">
-        <param name="movie" value="player_mp3_maxi.swf" />
+        <param name="movie" value="/player/player_mp3_maxi.swf" />
         <param name="FlashVars" value="mp3=#{mp3}&showvolume=#{show_volume}&showloading=#{show_loading}" />
       </object>
     )
