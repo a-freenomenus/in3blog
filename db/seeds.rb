18e96582a23267636e65b84c9b1963cec1aa6c9a
uzzz <max.gilinsky@gmail.net>
2010-01-04 11:36:29 +0200

    pages fixes and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
 User.create(:username => 'editor', :email => 'editor@domain.com',
-           :password => 'editor', :password_confirmation => 'editor')
-user = User.create(:username => 'administrator', :email => 'administrator@domain.com',
-           :password => 'administrator', :password_confirmation => 'administrator')
-user.admin = true
-user.save
+           :password => 'editor', :password_confirmation => 'editor') unless User.find_by_username('editor')
+
+unless User.find_by_username('administrator')
+  user = User.create(:username => 'administrator', :email => 'administrator@domain.com',
+             :password => 'administrator', :password_confirmation => 'administrator')
+  user.admin = true
+  user.save
+end
+
+unless Page.find_by_permalink('about')
+  Page.create(:permalink => 'about', :body => 'About this blog')
+end
