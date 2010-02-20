5ac41e431a5f1c8bbf09737583577d174ab88148
uzzz <uzzz@uzzz-laptop.(none)>
2010-02-20 14:17:38 +0200

    permalink for page not needed more

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,11 @@
 class Page < ActiveRecord::Base
   has_friendly_id :permalink
 
-  validates_presence_of :permalink, :body
-  validates_uniqueness_of :permalink
+  validates_presence_of :body
+  validates_uniqueness_of :permalink, :allow_blank => true
+
+  def to_param
+    permalink.blank? ? id.to_s : permalink
+  end
+
 end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -2,17 +2,25 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Page do
   
-  it "should require premalink and body" do
+  it "should require body" do
     page = Page.new
-    %w(body permalink).each do |attr|
-      page.should have(1).error_on(attr)
-    end
+    page.should have(1).error_on(:body)
   end
   
   it "should validate uniqueness of permalink" do
     Factory.create(:page, :permalink => 'foo')
     page = Factory.build(:page, :permalink => 'foo')
     page.should have(1).error_on(:permalink)
+  end
+
+  it "to_param sould be eqals id if permalink not setted" do
+    page = Factory.build(:page, :id => '1', :permalink => '')
+    page.to_param.should == '1'
+  end
+  
+  it "to_param sould be equals permalink if permalink setted" do
+    page = Factory.build(:page, :permalink => 'foo')
+    page.to_param.should == 'foo'
   end
 
 end
